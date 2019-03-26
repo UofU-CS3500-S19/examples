403f45d99dc96908efed2e55e495fe2cb8962b96
Joseph Zachary <zachary@cs.utah.edu>
2019-03-26 15:29:46 -0600

    Modified in lecture

--- ToDoListServer/ToDoListServer/Models/Item.cs
+++ ToDoListServer/ToDoListServer/Models/Item.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Web;
 
 namespace ToDoListServer.Models
@@ -8,12 +9,19 @@ namespace ToDoListServer.Models
     /// <summary>
     /// An item on the ToDo list
     /// </summary>
+    [DataContract]
     public class Item
     {
         /// <summary>
         /// ID of user who is placing item on the list
         /// </summary>
         public string UserID { get; set; }
+
+        /// <summary>
+        /// For demo purposes
+        /// </summary>
+        [DataMember(EmitDefaultValue = false)]
+        public int Age { get; set; }
 
         /// <summary>
         /// Description of item
