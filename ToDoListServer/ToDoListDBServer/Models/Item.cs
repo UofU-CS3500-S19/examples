3b0e84e6b1cccec3b80d062b077ca7a8fc04e881
Joseph Zachary <zachary@cs.utah.edu>
2019-04-01 11:22:46 -0600

    Add DB version

---
+++ ToDoListServer/ToDoListDBServer/Models/Item.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace ToDoListServer.Models
+{
+    /// <summary>
+    /// An item on the ToDo list
+    /// </summary>
+    [DataContract]
+    public class Item
+    {
+        /// <summary>
+        /// ID of user who is placing item on the list
+        /// </summary>
+        [DataMember(EmitDefaultValue = false)]
+        public string UserID { get; set; }
+
+        /// <summary>
+        /// Description of item
+        /// </summary>
+        public string Description { get; set; }
+    }
+}
